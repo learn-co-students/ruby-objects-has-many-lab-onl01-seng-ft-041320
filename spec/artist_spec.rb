b2a6a87d0ff907a43ba7610a0984a3d80fdbf100
webdevjscoder <cobianjuan19@gmail.com>
2020-04-28 10:10:23 -0700

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -20,6 +20,7 @@ describe "Artist" do
       it "has many songs" do
         expect(adele.songs).to be_a(Array)
         hello = Song.new("Hello")
+        adele.add_song(hello) #added this line
         hello.artist = adele
         expect(adele.songs).to eq([hello])
       end
